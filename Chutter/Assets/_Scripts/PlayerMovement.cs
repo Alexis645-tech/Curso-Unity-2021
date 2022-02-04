39c5aa92f98fd60f258e5876d80a04b21f1c6862
Alexis645-tech <castroalexis645@gmail.com>
2022-02-04 15:04:46 -0500

    Commit 3

--- Chutter/Assets/_Scripts/PlayerMovement.cs
+++ Chutter/Assets/_Scripts/PlayerMovement.cs
@@ -12,10 +12,12 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("Fuerza de rotación del personaje en newtons por segundo")]
     [Range(0, 360)][SerializeField]private float rotationSpeed;
 
-    private Rigidbody rb;
+    private Rigidbody _rigidbody;
+    private Animator _animator;
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
         
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -30,13 +32,34 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         Vector3 dir = new Vector3(horizontal, 0, vertical);
         //transform.Translate(dir.normalized*space);
-        rb.AddRelativeForce(dir.normalized*space);
+        _rigidbody.AddRelativeForce(dir.normalized*space);
 
         float angle = rotationSpeed * Time.deltaTime;
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
         //transform.Rotate(0, mouseX*angle, 0);
-        rb.AddRelativeTorque(0, mouseX*angle, 0);
+        _rigidbody.AddRelativeTorque(0, mouseX*angle, 0);
+        
+        _animator.SetFloat("Velocity", _rigidbody.velocity.magnitude);
 
+        /*
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _animator.SetFloat("Velocity", _rigidbody.velocity.magnitude);
+        }
+        else
+        {
+            if (Mathf.Abs(horizontal) < 0.01f && Mathf.Abs(vertical) < 0.01f)
+            {
+                _animator.SetFloat("Velocity", 0);
+            }
+            else
+            {
+                _animator.SetFloat("Velocity", 0.15f);
+            }
+        }
+
+        _animator.SetFloat("MoveX", horizontal);
+        _animator.SetFloat("MoveY", vertical);*/
     }
 }
--- Chutter/Assets/_Scripts/PlayerShoting.cs
+++ Chutter/Assets/_Scripts/PlayerShoting.cs
@@ -8,6 +8,9 @@ public class PlayerShoting : MonoBehaviour
     [SerializeField]private GameObject shootingPoint;
     private Animator _animator;
 
+    [SerializeField]private ParticleSystem fireEffect;
+    [SerializeField]private AudioSource shootSound;
+
     public int bulletsAmount;
 
     private void Start()
@@ -33,6 +36,13 @@ public class PlayerShoting : MonoBehaviour
         bullet.transform.position = shootingPoint.transform.position;
         bullet.transform.rotation = shootingPoint.transform.rotation;
         bullet.SetActive(true);
+        fireEffect.Play();
+        Instantiate(shootSound, transform.position, transform.rotation).GetComponent<AudioSource>().Play();
+        
         bulletsAmount--;
+        if (bulletsAmount < 0)
+        {
+            bulletsAmount = 0;
+        }
     }
 }
