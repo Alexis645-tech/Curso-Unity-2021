3c89681a6340ca224ee7a3fb5e77f10422162da3
Alexis645-tech <castroalexis645@gmail.com>
2022-02-05 14:43:54 -0500

    commit

--- Chutter/Assets/_Scripts/IA/Sight.cs
+++ Chutter/Assets/_Scripts/IA/Sight.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,4 +10,15 @@ public class Sight : MonoBehaviour
 
     [SerializeField]private LayerMask targetLayers;
     [SerializeField]private LayerMask obstacleLayers;
+
+    private void Update()
+    {
+        //filtro de distancia
+        Collider[] colliders = Physics.OverlapSphere(transform.position, distance, targetLayers);
+        foreach (var collider in colliders)
+        {
+            Vector3 directionToCollider = Vector3.Normalize(collider.bounds.center - transform.position);
+            float angleToCollider = Vector3.Angle(transform.forward, directionToCollider);
+        }
+    }
 }
