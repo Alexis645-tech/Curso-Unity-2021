4064182856083b5da331b4d14dfca6e84e9ad72c
Alexis645-tech <castroalexis645@gmail.com>
2022-02-07 07:12:50 -0500

    commit 1

--- Chutter/Assets/_Scripts/IA/Sight.cs
+++ Chutter/Assets/_Scripts/IA/Sight.cs
@@ -11,14 +11,37 @@ public class Sight : MonoBehaviour
     [SerializeField]private LayerMask targetLayers;
     [SerializeField]private LayerMask obstacleLayers;
 
+    [SerializeField]private Collider detectedTarget;
+
     private void Update()
     {
         //filtro de distancia
         Collider[] colliders = Physics.OverlapSphere(transform.position, distance, targetLayers);
+        detectedTarget = null;
         foreach (var collider in colliders)
         {
             Vector3 directionToCollider = Vector3.Normalize(collider.bounds.center - transform.position);
+            
+            //Angulo que forman el vector vision  con el vector objetivo
             float angleToCollider = Vector3.Angle(transform.forward, directionToCollider);
+            
+            //Si el angulo es menor que el de vision
+            if (angleToCollider < angle)
+            {
+                //Comprobamos que en la linea de vision enemigo -> objetivo no haya obstaculos
+                if (!Physics.Linecast(transform.position, collider.bounds.center, obstacleLayers))
+                {
+                    //Guardamos la referencia del objetivo detectado
+                    detectedTarget = collider;
+                    break;
+                }
+            }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, distance);
     }
 }
