cd154d3093f956f141c4a6197a689aaa20328338
Alexis645-tech <castroalexis645@gmail.com>
2022-02-11 08:00:43 -0500

    commit 5

--- Pokymon/Assets/_Scripts/PlayerController.cs
+++ Pokymon/Assets/_Scripts/PlayerController.cs
@@ -1,27 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     private bool isMoving;
     
     [SerializeField]private float speed;
     private Vector2 input;
-    
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         if(!isMoving)
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
+            if (input.x != 0)
+            {
+                input.y = 0;
+            }
             if (input != Vector2.zero)
             {
+                _animator.SetFloat("MoveX", input.x);
+                _animator.SetFloat("MoveY", input.y);
                 var targetPosition = transform.position;
                 targetPosition.x += input.x;
                 targetPosition.y += input.y;
+                StartCoroutine(MoveTowards(targetPosition));
             }
         }
-        
+    }
+    
+    IEnumerator MoveTowards(Vector3 destination)
+    {
+        isMoving = true;
+        while(Vector3.Distance(transform.position, destination) > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = destination;
+        isMoving = false;
     }
 }
